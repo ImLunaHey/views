5d0f0a416c7070776f099cdfb0f215dcf62e48ea
ImLunaHey <105922175+ImLunaHey@users.noreply.github.com>
2023-06-01 05:12:19 +0930

    feat: allow every single permission

--- src/index.ts
+++ src/index.ts
@@ -73,6 +73,12 @@ const getLocation = async (ip: string) => {
 // Make shit more secure
 app.use(helmet());
 
+// Allow every single permission
+app.use((_request, response, next) => {
+    response.setHeader('Permissions-Policy', 'Permissions-Policy: accelerometer=(self), ambient-light-sensor=(self), autoplay=(self), battery=(self), camera=(self), cross-origin-isolated=(self), display-capture=(self), document-domain=(self), encrypted-media=(self), execution-while-not-rendered=(self), execution-while-out-of-viewport=(self), fullscreen=(self), geolocation=(self), gyroscope=(self), keyboard-map=(self), magnetometer=(self), microphone=(self), midi=(self), navigation-override=(self), payment=(self), picture-in-picture=(self), publickey-credentials-get=(self), screen-wake-lock=(self), sync-xhr=(self), usb=(self), web-share=(self), xr-spatial-tracking=(self), clipboard-read=(self), clipboard-write=(self), gamepad=(self), speaker-selection=(self), conversion-measurement=(self), focus-without-user-activation=(self), hid=(self), idle-detection=(self), interest-cohort=(self), serial=(self), sync-script=(self), trust-token-redemption=(self), unload=(self), window-placement=(self), vertical-scroll=(self)');
+    next();
+});
+
 // Since were using a reverse proxy
 // we need this to get the correct client IP
 app.enable('trust proxy');
